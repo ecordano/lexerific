2628dbd1804fc363af893d021d7015e0b1a83daa
ecordano <emma.cordano@lover.ly>
2015-06-03 17:56:35 -0400

    added inital TreeNode test

---
+++ test/TreeNodeTest.js
@@ -0,0 +1,49 @@
+var chai = require('chai');
+var expect = chai.expect;
+var sinon = require('sinon');
+
+var TreeNode = require('../lib/TreeNode');
+
+describe('TreeNode', function() {
+	describe('constructor', function() {
+		it ('Should pass as all properties are private', function() {
+		
+		});
+	});
+	describe('isLeaf', function() {
+		it ('Should ', function() {
+		
+		});
+		//expect();
+	});
+	describe('addChild', function() {
+		it ('Should ', function() {
+		
+		});
+		//expect();
+	});
+	describe('setAttribute', function() {
+		it ('Should ', function() {
+		
+		});
+		//expect();
+	});
+	describe('getAttribute', function() {
+		it ('Should ', function() {
+		
+		});
+		//expect();
+	});
+	describe('getChildren', function() {
+		it ('Should ', function() {
+		
+		});
+		//expect();
+	});
+	describe('walk', function() {
+		it ('Should ', function() {
+		
+		});
+		//expect();
+	});
+});
